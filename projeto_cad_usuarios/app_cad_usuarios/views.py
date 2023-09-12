a01a994c72dd811404478c9f4ceb9a279fb49e3c
Fernando Fábner <fernandofabner@gmail.com>
2023-09-12 19:52:58 -0300

    terminando a parte inicial

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def home(request):
     return render(request, 'usuarios/home.html')
+
+def usuarios(request):
+    #salvar os dados da tela pa o banco de dados.
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    # Exibir todos os usuarios ja cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    #retornar os dados para a página de listagem de usuários
+    return render(request, 'usuarios/usuarios.html',usuarios)
--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -5,4 +5,7 @@ from app_cad_usuarios import views
 urlpatterns = [
     # rota, view responsável, nome de referência
     path('', views.home, name='home'),
+    #usuarios.com/usuarios
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
+    
 ]
